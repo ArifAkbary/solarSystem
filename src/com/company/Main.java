02bddc5965c21be13135a7f0a6d9edd7d7170a48
ArifAkbary <arifakbary022@gmail.com>
2022-08-27 22:42:12 -0700

    sorlar system ready to deplyed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+solarSystem inception=new solarSystem();
+inception.Sun=1;
+inception.Moon=4;
+inception.Plants=2;
+        System.out.println("the basic structure is ready ");
 
 
     }
