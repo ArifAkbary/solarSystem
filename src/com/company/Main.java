b640a3610789efacd4b30d5b7598e5a80d5d606e
ArifAkbary <arifakbary022@gmail.com>
2022-08-27 23:07:51 -0700

    feature sun added with errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,10 +9,11 @@ inception.Sun=1;
 inception.Moon=4;
 inception.Plants=2;
         System.out.println("the basic structure is ready ");
-
-
+//devloping the future sun
+        SUN om=new SUN();
+        om.temprature="557";
+        om.color="pink";
+        om.name="mula";
+        om.radius="12333;
     }
-
-
-
 }
